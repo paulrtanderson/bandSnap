32a37597f0da2c9a5ada1302034f57539e99ee77
Paul Anderson <2726368a@student.gla.ac.uk>
2024-03-05 18:38:45 +0000

    added rudimentary views

--- bandsnap/urls.py
+++ bandsnap/urls.py
@@ -4,4 +4,8 @@ from bandsnap import views
 app_name = 'bandsnap'
 urlpatterns = [
 path('', views.index, name='index'),
+path('about/', views.about, name='about'),
+path('search/', views.search, name='search'),
+path('login/', views.login, name='login'),
+path('signup/', views.signup, name='signup'),
 ]
--- bandsnap/views.py
+++ bandsnap/views.py
@@ -4,3 +4,15 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Welcome to bandsnap!")
+
+def signup(request):
+    return HttpResponse("this will be the signup page")
+
+def login(request):
+    return HttpResponse("this will be the login page")
+
+def search(request):
+    return HttpResponse("this will be the search page")
+
+def about(request):
+    return HttpResponse("this will be the about page")
