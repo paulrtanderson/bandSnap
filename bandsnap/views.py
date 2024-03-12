b8f8b1300009d5d6120fb907068d89cb0a891c9c
Paul Anderson <2726368a@student.gla.ac.uk>
2024-03-12 00:54:14 +0000

    created navbar

--- bandsnap/views.py
+++ bandsnap/views.py
@@ -3,21 +3,21 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    context_dict = {}
+    context_dict = {'active_link': 'index'}
     return render(request,'bandsnap/index.html',context=context_dict)
 
 def signup(request):
-    context_dict = {}
+    context_dict = {'active_link': 'signup'}
     return render(request,'bandsnap/signup.html',context=context_dict)
 
 def login(request):
-    context_dict = {}
+    context_dict = {'active_link': 'login'}
     return render(request,'bandsnap/login.html',context=context_dict)
 
 def search(request):
-    context_dict = {}
+    context_dict = {'active_link': 'search'}
     return render(request,'bandsnap/search.html',context=context_dict)
 
 def about(request):
-    context_dict = {}
+    context_dict = {'active_link': 'about'}
     return render(request,'bandsnap/about.html',context=context_dict)
