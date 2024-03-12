4c34945b8b860b3e8743f3af4af0a5b0a3e63edd
Aysha <2800957k@student.gla.ac.uk>
2024-03-12 14:56:36 +0000

    restricted page in view updated

--- wad_group_project_bandsnap/settings.py
+++ wad_group_project_bandsnap/settings.py
@@ -132,3 +132,4 @@ MEDIA_ROOT = MEDIA_DIR
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGIN_URL = 'bandsnap:login'
